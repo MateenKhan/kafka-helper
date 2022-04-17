972da3730710c87e75958a707fd72225cb738ad3
mateen <makjavaprogrammer@gmail.com>
2022-04-17 05:05:00 -0700

    updated topic partition rendering logic

--- src/main/resources/static/js/bstreeview.js
+++ src/main/resources/static/js/bstreeview.js
@@ -15,11 +15,11 @@
      */
     var pluginName = "bstreeview",
         defaults = {
-            expandIcon: 'fa fa-angle-down fa-fw',
-            collapseIcon: 'fa fa-angle-right fa-fw',
-            expandClass: 'show',
+            expandIcon: "fa fa-angle-down fa-fw",
+            collapseIcon: "fa fa-angle-right fa-fw",
+            expandClass: "show",
             indent: 1.25,
-            parentsMarginLeft: '1.25rem',
+            parentsMarginLeft: "1.25rem",
             openNodeLinkOnNewTab: true
 
         };
@@ -27,11 +27,11 @@
      * bstreeview HTML templates.
      */
     var templates = {
-        treeview: '<div class="bstreeview"></div>',
-        treeviewItem: '<div role="treeitem" class="list-group-item" data-bs-toggle="collapse"></div>',
-        treeviewGroupItem: '<div role="group" class="list-group collapse" id="itemid"></div>',
-        treeviewItemStateIcon: '<i class="state-icon"></i>',
-        treeviewItemIcon: '<i class="item-icon"></i>'
+        treeview: "<div class="bstreeview"></div>",
+        treeviewItem: "<div role="treeitem" class="list-group-item" data-bs-toggle="collapse"></div>",
+        treeviewGroupItem: "<div role="group" class="list-group collapse" id="itemid"></div>",
+        treeviewItemStateIcon: "<i class="state-icon"></i>",
+        treeviewItemIcon: "<i class="item-icon"></i>"
     };
     /**
      * BsTreeview Plugin constructor.
@@ -63,29 +63,29 @@
                 delete this.settings.data;
             }
             // Set main bstreeview class to element.
-            $(this.element).addClass('bstreeview');
+            $(this.element).addClass("bstreeview");
 
             this.initData({ nodes: this.tree });
             var _this = this;
             this.build($(this.element), this.tree, 0);
             // Update angle icon on collapse
-            $(this.element).on('click', '.list-group-item', function (e) {
-                $('.state-icon', this)
+            $(this.element).on("click", ".list-group-item", function (e) {
+                $(".state-icon", this)
                     .toggleClass(_this.settings.expandIcon)
                     .toggleClass(_this.settings.collapseIcon);
                 // navigate to href if present
-                if (e.target.hasAttribute('href')) {
+                if (e.target.hasAttribute("href")) {
                     if (_this.settings.openNodeLinkOnNewTab) {
-                        window.open(e.target.getAttribute('href'), '_blank');
+                        window.open(e.target.getAttribute("href"), "_blank");
                     }
                     else {
-                        window.location = e.target.getAttribute('href');
+                        window.location = e.target.getAttribute("href");
                     }
                 }
                 else
                 {
                     // Toggle the data-bs-target. Issue with Bootstrap toggle and dynamic code
-                    $($(this).attr("data-bs-target")).collapse('toggle');
+                    $($(this).attr("data-bs-target")).collapse("toggle");
                 }
             });
         },
@@ -127,9 +127,9 @@
             $.each(nodes, function addNodes(id, node) {
                 // Main node element.
                 var treeItem = $(templates.treeviewItem)
-                    .attr('data-bs-target', "#" + _this.itemIdPrefix + node.nodeId)
-                    .attr('style', 'padding-left:' + leftPadding)
-                    .attr('aria-level', depth);
+                    .attr("data-bs-target", "#" + _this.itemIdPrefix + node.nodeId)
+                    .attr("style", "padding-left:" + leftPadding)
+                    .attr("aria-level", depth);
                 // Set Expand and Collapse icones.
                 if (node.nodes) {
                     var treeItemStateIcon = $(templates.treeviewItemStateIcon)
@@ -146,7 +146,7 @@
                 treeItem.append(node.text);
                 // Reset node href if present
                 if (node.href) {
-                    treeItem.attr('href', node.href);
+                    treeItem.attr("href", node.href);
                 }
                 // Add class to node if present
                 if (node.class) {
@@ -154,7 +154,7 @@
                 }
                 // Add custom id to node if present
                 if (node.id) {
-                    treeItem.attr('id', node.id);
+                    treeItem.attr("id", node.id);
                 }
                 // Attach node to parent.
                 parentElement.append(treeItem);
@@ -162,7 +162,7 @@
                 if (node.nodes) {
                     // Node group item.
                     var treeGroup = $(templates.treeviewGroupItem)
-                        .attr('id', _this.itemIdPrefix + node.nodeId);
+                        .attr("id", _this.itemIdPrefix + node.nodeId);
                     parentElement.append(treeGroup);
                     _this.build(treeGroup, node.nodes, depth);
                     if (node.expanded) {
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,46 +1,101 @@
 let baseUrl = "http://localhost:8080"
-let treeData = [];
+let partitionsHtml = '<div class="bg-dark p-2 partition-container">'+
+                            '<span class="text-white">partition-0</span>'+
+                            '<i class="bi bi-play-circle-fill text-primary float-right"></i>'+
+                        '</div>';
+
+let topicDiv = '<div id="topic-partition-container-1">' +
+                    '<a class="pl-0 nav-link active h4" data-target="#partitions" data-toggle="collapse" href="#" id="topic-label"></a>' +
+                    '<div class="collapse" id="partitions">' +
+                        '<div class="bg-dark p-2 partition-container">' +
+                            '<span class="text-white">partition-0</span>' +
+                            '<i class="bi bi-play-circle-fill text-primary float-right"></i>' +
+                        '</div>'+
+                    '</div>' +
+                '</div>';
 
 $(document).ready(function () {
-    getPartitions();
+
+    getTopics();
+    $("#load-topics").click(function (){
+         getTopics();
+
+    });
+
+    $(".collapse").on("show.bs.collapse", function () {
+            $(".collapse").collapse("hide");
+            getPartitions(this);
+    })
+
 });
 
-function getPartitions() {
-    $("#spinner").show(200)
+function getPartitions(ths){
+    $("#spinner").show(500);
+    let partitionsDiv = $(ths);
+    let topic = partitionsDiv.parent().find("a").attr("id");
     $.ajax({
-        method: "GET", url: baseUrl + "/topics/partitions"
+        method: "GET", url: baseUrl + "/topics/"+topic+"/partitions/"
+    }).done(function (data) {
+        updatePartitionsHTML(data,partitionsDiv,topic);
+    }).fail(function (e) {
+        console.log("error", e);
+        onError(e);
+    }).always(function (data) {
+        console.log("complete");
+        $("#spinner").hide(500);
+    });
+
+}
+
+// '<div class="bg-dark p-2 partition-container">'+
+// '<span class="text-white">partition-0</span>'+
+// '<i class="bi bi-play-circle-fill text-primary float-right"></i>'+
+// '</div>';
+
+function updatePartitionsHTML(data,partitionsDiv,topic){
+    if(data){
+        for(let i=0;i<data.length;i++){
+            let partitionElem = $("#partition-container").clone(true,true);
+            partitionElem.find(".text-white").text(topic+"-"+data[i]);
+            partitionElem.appendTo(partitionsDiv);
+        }
+    }
+}
+
+function getTopics() {
+    $("#spinner").show(500)
+    $.ajax({
+        method: "GET", url: baseUrl + "/topics"
     }).done(function (data) {
         updateTopics(data);
     }).fail(function (e) {
         console.log("error", e);
+        onError(e);
     }).always(function (data) {
         console.log("complete");
-        $("#spinner").hide(200);
+        $("#spinner").hide(500);
     });
 }
 
-function buildTreeWithData() {
-    $('#tree').bstreeview({
-        data: treeData, indent: 1.25, parentsMarginLeft: '1.25rem', openNodeLinkOnNewTab: true
-    });
+function onError(e){
+    $(".toast-body").text(e.statusText)
+    $(".toast").toast("show");
 }
 
-
-function updateTopics(data) {
-    prepareData(data);
-    buildTreeWithData();
-}
-
-function prepareData(data) {
-    let result = [];
-    if (data) for (const [key, value] of Object.entries(data)) {
-        let nodes = [];
-        let item = {"text": key, "icon": "fa fa-inbox fa-fw", "nodes": nodes}
-        for (let i = 0; i < value.length; i++) {
-            let node = {"text": key + "-" + value[i], "icon": "fa fa-inbox fa-fw"};
-            nodes[i] = node;
+function updateTopics(topics){
+    if(topics){
+        for(let i=0;i<topics.length;i++){
+            let topicElement = $("#topic-partition-container").clone(true,true);
+            topicElement.attr("id","topic-partition-container-"+i);
+            topicElement.find("#topic-label")
+                .attr("id",topics[i])
+                .attr("data-target","#partitions-"+i)
+                .text(topics[i]);
+            topicElement.find("#partitions").attr("id","partitions-"+i)
+            topicElement.appendTo("#topics-container");
+            topicElement.show(1000);
         }
-        result[result.length] = item;
     }
-    treeData = result;
+
 }
+
